307e6816df75868ff7781be1cbe0af4136c71864
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-10-13 11:31:08 -0700

    hid login buttons if already logged in

--- src/components/HomeHeading/HomeHeading.js
+++ src/components/HomeHeading/HomeHeading.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Typography } from "@material-ui/core";
-import "./HomeHeading.css";
 import LoginButtons from "../LoginButtons/LoginButtons";
 
 const HomeHeading = () => {
+	// email can only be found if logged in
+	const { email } = useSelector((st) => ({ email: st.login.email }));
 	return (
 		<>
 			<Typography
@@ -23,7 +25,8 @@ const HomeHeading = () => {
 			>
 				Your one stop shop for all of your project outsourcing needs
 			</Typography>
-			<LoginButtons />
+			{/* only show login buttons if user is not logged in */}
+			{!email ? <LoginButtons /> : null}
 		</>
 	);
 };
