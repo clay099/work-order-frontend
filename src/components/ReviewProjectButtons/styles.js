ccd4fdd500cfcce0e896f9fb2e7de7c56fe1a30f
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-10-15 15:07:36 -0700

    moved styled logic to seperate folder

--- src/components/ReviewProjectButtons/styles.js
+++ src/components/ReviewProjectButtons/styles.js
@@ -1,17 +1,4 @@
 import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-	button: {
-		marginTop: theme.spacing(2),
-	},
-	sliderTitle: {
-		marginTop: theme.spacing(2),
-	},
-}));
-
-export default useStyles;
-
-import { makeStyles } from "@material-ui/core/styles";
 import { styledButton, sliderTitle } from "../../styles/styleObjects";
 
 const useStyles = makeStyles((theme) => {
