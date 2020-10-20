d0c55bbb6acb5c266f78680dc47f1bd2458fe5df
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-10-20 14:43:30 -0700

    added function to see tradesmen details and ratings

--- src/components/TradesmenDetails/TradesmenDetails.js
+++ src/components/TradesmenDetails/TradesmenDetails.js
@@ -4,7 +4,34 @@ import { useSelector, useDispatch, shallowEqual } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { getTradesmenDetailsFromAPI } from "../../actions/tradesmen";
 import Loading from "../Loading/Loading";
+import TableHeadings from "../TableHeadings/TableHeadings";
+import {
+	Table,
+	Typography,
+	Paper,
+	TableContainer,
+	TableRow,
+	TableCell,
+	TableBody,
+} from "@material-ui/core/";
+import useStyles from "./styles";
 
+/** TradesmenDetails Component
+ *
+ * Gets the Redux state for Token and tradesmen details
+ *
+ * Sends a request to the API to get latest tradesmen details
+ *
+ * Displays a loading page while API request occurs
+ *
+ * When Logged in and Loading has finished, renders two tables:
+ *    - Table 1 contains
+ *        - Tradesmen Details
+ *        - Tradesmen Rating
+ *
+ *     - Table 2 contains
+ *        - Tradesmen reviews for each project completed
+ */
 const TradesmenDetails = () => {
 	const { id } = useParams();
 	const { token, details } = useSelector(
@@ -17,6 +44,8 @@ const TradesmenDetails = () => {
 
 	const dispatch = useDispatch();
 	const [isLoading, setIsLoading] = useState(true);
+
+	const classes = useStyles();
 
 	useEffect(
 		function () {
@@ -41,7 +70,80 @@ const TradesmenDetails = () => {
 
 	if (isLoading) return <Loading />;
 
-	return <div>TradesmenDetails go here</div>;
+	const headingList = [
+		"ID",
+		"First Name",
+		"Last Name",
+		"Email",
+		"Phone",
+		"Rating",
+	];
+
+	return (
+		<>
+			<TableContainer component={Paper} className={classes.table}>
+				<Typography
+					component="h2"
+					variant="h6"
+					color="primary"
+					gutterBottom
+				>
+					{`${details.first_name} ${details.last_name} Details`}
+				</Typography>
+				<Table size="small" dense table>
+					<TableHeadings headings={headingList} />
+					<TableBody>
+						<TableRow>
+							<TableCell>{details.id}</TableCell>
+							<TableCell>{details.first_name}</TableCell>
+							<TableCell>{details.last_name}</TableCell>
+							<TableCell>{details.email}</TableCell>
+							<TableCell>{details.phone}</TableCell>
+							<TableCell>
+								{details.rating === null
+									? "not yet rated"
+									: `${Number(+details.rating).toFixed(
+											2
+									  )} / 10`}
+							</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</TableContainer>
+
+			{details.review_comments[0] ? (
+				<TableContainer component={Paper} className={classes.table}>
+					<Typography
+						component="h2"
+						variant="h6"
+						color="primary"
+						gutterBottom
+					>
+						{`${details.first_name} ${details.last_name} Project Reviews`}
+					</Typography>
+					<Table size="small" dense table>
+						<TableHeadings headings={["Reviews"]} />
+						<TableBody>
+							{details.review_comments.map((review) => (
+								<TableRow>
+									<TableCell>{review}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			) : (
+				<Typography
+					component="h2"
+					variant="h6"
+					color="primary"
+					gutterBottom
+				>
+					{`${details.first_name} ${details.last_name} does not have any project reviews`}
+				</Typography>
+			)}
+		</>
+	);
 };
 
 export default TradesmenDetails;
---
+++ src/components/TradesmenDetails/styles.js
@@ -0,0 +1,8 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { table } from "../../styles/styleObjects";
+
+const useStyles = makeStyles((theme) => {
+	return table(theme);
+});
+
+export default useStyles;
